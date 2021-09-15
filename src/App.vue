3ee7c0aaf2fb4f4d6350055867685c3d087eb4aa
MV-GH <maarten.vercruysse@outlook.be>
2021-09-15 20:00:56 +0200

    Add header component and some subsections

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,24 @@
   <main>
     <aside><nav><p>nav</p></nav></aside>
     <article>
-      <h2>Projects</h2>
+      <SubHeader :title="'Still in progress'" />
+      <p>
+        This site is still in progress, There is gonna be many changes.
+        Such as:
+      </p>
+      <ul>
+        <li>Mobile friendly</li>
+        <li>Theme</li>
+        <li>Adding navigation</li>
+        <li>General Refinements</li>
+      </ul>
+      <SubHeader :title="'Projects'" />
       <ProjectList :projects="projectRefs"/>
+      <SubHeader :title="'Future projects'" />
+      <p>
+        After finishing this site and my open source lua projects (on my public account).
+        I'm planning to learn a low level programming language such as c++ or rust. And build a few projects with it.
+      </p>
     </article>
     <div><p>t</p></div>
   </main>
@@ -14,6 +30,7 @@
 
 <script lang="ts">
 import ProjectList from "./components/ProjectList.vue";
+import SubHeader from "./components/Header.vue";
 import _Projects from "./assets/data/projects.json"
 import ProjectType from "./Types/Project"
 import {defineComponent, ref} from "vue";
@@ -22,7 +39,7 @@ const projects = _Projects as ProjectType[]
 
 export default defineComponent({
   name: "App",
-  components: { ProjectList },
+  components: { ProjectList, SubHeader},
   setup(){
     const projectRefs = ref<ProjectType[]>(projects);
     return {
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <h2>{{ title }}</h2>
+  <hr>
+</template>
+
+<script lang="ts">
+
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  props: {
+    title: {
+      required: true,
+      type: String
+    }
+  }
+})
+
+</script>
+
+<style scoped lang="scss">
+$color: whitesmoke;
+h2 {
+  text-align: center;
+}
+
+hr {
+  background: $color;
+  border: 0;
+  height: 1px;
+  width: 10%;
+  position: relative;
+  margin: 30px auto;
+
+  &:before {
+    content: "";
+    width: 6px;
+    height: 6px;
+    background: $color;
+    display: inline-block;
+    border: 2px solid $color;
+    position: absolute;
+    top: -5px;
+    left: 50%;
+    margin: 0 0 0 -3px;
+    transform: rotate(45deg);
+  }
+}
+</style>
